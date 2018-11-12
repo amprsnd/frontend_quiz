b35e0345b5a5b8861ce86c883b886ea717b1ee8a
Amprsnd <ampersand@pseudostudio.info>
2018-11-12 15:40:22 +0600

    parser

---
+++ app.rb
@@ -0,0 +1,46 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'nokogumbo'
+require 'json'
+
+class Parser < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    quiz_data = {}
+    quiz_html = Nokogiri::HTML5.get('http://davidshariff.com/quiz/')
+  
+    quiz_html.css('.skill-area').each do |area|
+      # each skill area
+      skill = area['data-skill'].to_s.downcase
+      quiz_data[skill] = []
+
+      # each questions in skill area
+      area.css('article').each_with_index do |q, i|
+        if q['class'].nil? || q['class'] == 'no-code-sample'
+          # question text and code
+          question = {
+            code: q.css('.prettyprint'),
+            question: q.css('.question').inner_html,
+            answers: []
+          }
+          # each answers
+          q.css('.answers li').each_with_index do |a, i|
+            question[:answers] << {
+              answer: a.css('span').inner_html,
+              correct: a.css('.correct-btn').length > 0 ? true : false
+            }
+          end
+          quiz_data[skill] << question
+        end
+      end
+    end
+    content_type :json
+    JSON.pretty_generate(quiz_data)
+  end
+
+end
+
+Parser.run!
